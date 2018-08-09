448a9a2e07aa4d8fc1263e874c8459e7dc2674fd
Brian Faure <bfaure23@gmail.com>
2018-08-09 18:30:18 -0400

    fixed issue where not converted to string

--- rest_api_server.py
+++ rest_api_server.py
@@ -3,6 +3,7 @@ from flask import Flask,request
 from flask_restful import Resource,Api
 from json import dumps
 from flask_jsonpify import jsonify
+from time import time
 
 app=Flask(__name__)
 api=Api(app)
@@ -14,23 +15,25 @@ def load_data():
     print("Loading data...")
     f=open(fname,'r')
     data={}
+    t0=time()
     for line in f:
         items=line.split("\t")
         if len(items)==2:
             data[items[0]]=items[1].strip()
+    print("Loaded data in %d seconds."%int(time()-t0))
     return data 
 
 def get_results(query):
     ct=0
-    if query in data:     ct+=data[query]
-    if query+'/' in data: ct+=data[query+'/']
+    if query in data:     ct+=int(data[query])
+    if query+'/' in data: ct+=int(data[query+'/'])
     return ct
 
 class LinkCt(Resource):
     def get(self,url): 
         true_url=url.replace("&slash;","/")
         linked_to_ct=get_results(true_url)
-        return {'count':linked_to_ct}
+        return {'count':str(linked_to_ct)}
 
 api.add_resource(LinkCt,'/count/<url>')
 
